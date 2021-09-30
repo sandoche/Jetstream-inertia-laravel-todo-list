d97469696b1de28e397f9d6ab32f29d869a8a148
Sandoche Adittane <sandoche@protonmail.com>
2021-09-30 11:54:51 +0100

    :construction: Work in progress todo item

--- resources/js/Components/TodoItem.vue
+++ resources/js/Components/TodoItem.vue
@@ -1,13 +1,22 @@
 <template>
-  <li><slot></slot></li>
+  <li :class="{ 'line-through': is_done }">
+    <input type="checkbox" :id="id" /> 
+    <label :for="id">
+      <slot></slot>
+    </label>
+  </li>
 </template>
 
 <script>
+import Label from "../Jetstream/Label.vue";
 export default {
-  name: 'TodoItem'
-}
+  name: "TodoItem",
+  props: {
+    id: Number,
+    is_done: Boolean,
+  },
+};
 </script>
 
 <style>
-
 </style>
